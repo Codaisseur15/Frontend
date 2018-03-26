cc727e50e601616fceef3937b0d04d18d17d0efa
oksmelnik <melnik.oksana@gmail.com>
2018-03-26 13:29:54 +0200

    lib

---
+++ src/lib/teacherQuiz.js
@@ -0,0 +1,19 @@
+export resultTeacher = {
+{
+name: GraphQl,
+{
+  classNumber: 15,
+  studentsNumber: 25,
+  studentsAnswers: 20,
+  avScore: 76,
+  totalScore: 100
+},
+{
+  classNumber: 14,
+  studentsNumber: 18,
+  studentsAnswers: 21,
+  avScore: 50,
+  totalScore: 80
+},
+},
+}
