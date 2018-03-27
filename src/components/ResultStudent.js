30aab861520b7e3aae98f9b77794040485f20c86
Bot13 <ruhtarduhd@gmail.com>
2018-03-27 18:06:15 +0200

    add QuizCreatorPage

---
+++ src/components/ResultStudent.js
@@ -0,0 +1,46 @@
+import React, {PureComponent} from 'react'
+import {connect} from 'react-redux'
+import StudentSummary from './StudentSummary'
+import QuizResultStudent from './QuizResultStudent'
+import { showStudentResult, showStudentQuiz } from '../actions/result'
+import PropTypes from 'prop-types'
+
+
+
+class ResultStudent extends PureComponent {
+  static propTypes = {
+    showStudentResult: PropTypes.func.isRequired,
+    showStudentQuiz: PropTypes.func.isRequired
+  }
+
+  componentWillMount() {
+    this.props.showStudentQuiz(),
+    this.props.showStudentResult()
+  }
+
+  render() {
+    let {student, studentQuiz} = this.props
+
+    return (
+      <div>
+        <h1>Quiz result for student</h1>
+        <StudentSummary student={student}/>
+        <QuizResultStudent studentQuiz={studentQuiz}/>
+      </div>
+    )
+  }
+}
+
+
+const mapStateToProps = (state, props) => ({
+  student: state.student,
+  studentQuiz: state.studentQuiz
+})
+
+export default connect(
+  mapStateToProps,
+  {
+    showStudentResult,
+    showStudentQuiz
+  })
+  (ResultStudent)
